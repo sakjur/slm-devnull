616259e56b48326f0158d5cad23d718b3c65d1ee
Jonathan Gill <mrjonathangill@gmail.com>
2013-02-02 09:52:28 +0100

    starsdata fetched

--- Challange 6/app.py
+++ Challange 6/app.py
@@ -1,9 +1,15 @@
 from flask import Flask, render_template
+from urllib import request
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def hello():
-    return render_template('starsystem.html')
+    url = request.urlopen("https://lostinspace.lanemarknad.se:8000/api2/?session=f6319047-1cfb-4bfa-ae4b-318355d2b90e&command=longrange")
+    stars = url.read().decode('utf-8')
+
+    return render_template('starsystem.html', stars=stars)
 
 if __name__ == "__main__":
     app.debug = True

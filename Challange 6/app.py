35f26a6c91251da24ec545b7da1792b5daeebacf
Jonathan Gill <mrjonathangill@gmail.com>
2013-02-02 10:23:00 +0100

    Added ship api

--- Challange 6/app.py
+++ Challange 6/app.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    url = urllib2.urlopen("https://lostinspace.lanemarknad.se:8000/api2/?session=f6319047-1cfb-4bfa-ae4b-318355d2b90e&command=longrange")
-    stars = url.read().decode('utf-8')
+    urlstars = urllib2.urlopen("https://lostinspace.lanemarknad.se:8000/api2/?session=f6319047-1cfb-4bfa-ae4b-318355d2b90e&command=longrange")
+    stars = urlstars.read().decode('utf-8')
 
-    return render_template('starsystem.html', stars=stars)
+    urlship = urllib2.urlopen("https://lostinspace.lanemarknad.se:8000/api2/?session=f6319047-1cfb-4bfa-ae4b-318355d2b90e&command=ship&arg=show")
+    ship = urlship.read().decode('utf-8')
+
+    return render_template('starsystem.html', stars=stars, ship=ship)
 
 if __name__ == "__main__":
     app.debug = True

610cd342b33acc186b22ff171f0ed5e0f3ee624a
Emil Tullstedt <sakjur@gmail.com>
2013-02-02 11:10:28 +0100

    Some awesomeness

--- Challange 6/app.py
+++ Challange 6/app.py
@@ -9,16 +9,26 @@ sid = "f6319047-1cfb-4bfa-ae4b-318355d2b90e"
 @app.route("/<starchoice>")
 def hello(starchoice=None):
     urlstars = urllib2.urlopen(uri + "/api2/?session=" + sid + "&command=longrange")
-    stars = urlstars.read().decode('utf-8')
+    stars = urlstars.read()
 
     urlship = urllib2.urlopen(uri + "/api2/?session=" + sid + "&command=ship&arg=show")
-    ship = urlship.read().decode('utf-8')
+    ship = urlship.read()
 
     if starchoice:
         starchoice = starchoice.replace(" ", "%20")
         urllib2.urlopen(uri + "/api2/?session=" + sid + "&command=ship&arg=setunidest&arg2=" + starchoice)
 
-    return render_template('starsystem.html', stars=stars, ship=ship, starchoice=starchoice)
+    return render_template('starsystem.html', stars=stars, ship=ship)
+
+@app.route("/current")
+def currentSystem():
+    urlshort = urllib2.urlopen(uri + "/api2/?session=" + sid + "&command=shortrange")
+    short = urlshort.read()
+
+    urlship = urllib2.urlopen(uri + "/api2/?session=" + sid + "&command=ship&arg=show")
+    ship = urlship.read()
+
+    return render_template('planetsystem.html', short=short, ship=ship)
 
 if __name__ == "__main__":
     app.debug = True
--- Challange 6/static/main.js
+++ Challange 6/static/main.js
@@ -1,6 +1,7 @@
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+
 function jsonToMap(json, shipson) {
-    var canvas = document.getElementById('canvas');
-    var ctx = canvas.getContext('2d');
 
     for (var i = 0; i < json.stars.length; i++)
     {
@@ -12,10 +13,27 @@ function jsonToMap(json, shipson) {
         $("#starlist").append("<a href=\"/" + star.name + "\">" + star.name + "</a><br />");
     };
 
-    console.log (shipson);
     ctx.fillStyle = "rgb(0,0,200)"
     ctx.fillRect(shipson.unix, shipson.uniy, 4, 4)
 
     setTimeout(location.reload,3000)
 
+}
+
+function awesomeToMap(awesome) {
+    ctx.fillStyle = "rgb(200, 100, 0)";
+    ctx.fillRect(100,100,10,10);
+
+    for (var i = 0; i < json.awesome.length; i++)
+    {
+        planet = json.awesome[i];
+
+        ctx.fillStyle = "rgb(200,0,0)";
+        ctx.fillRect(planet.x, planet.y, 4, 4);
+
+        $("#starlist").append("<a href=\"/" + star.name + "\">" + star.name + "</a><br />");
+    };
+    
+    ctx.fillStyle = "rgb(0,0,200)"
+    ctx.fillRect(shipson.unix, shipson.uniy, 4, 4)
 }
